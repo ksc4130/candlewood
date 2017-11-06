08808026b01bbeeeced9c43a2bb811d4fdc630c3
Kyle Curren <ksc4130@gmail.com>
2017-11-06 12:01:40 -0500

    filter types

--- client/app/js/controllers/documentsCtrl.js
+++ client/app/js/controllers/documentsCtrl.js
@@ -1,23 +1,25 @@
-(function(){
-    'use strict';
-    angular.module('cwl.core')
-        .controller('documentsCtrl', documentsCtrl);
-    documentsCtrl.$inject = ['$scope', 'authSrv', '$location', 'docs', 'documentSrv', 'hideTypeNav'];
-    function documentsCtrl($scope, authSrv, $location, docs, documentSrv, hideTypeNav){
-      $scope.hideTypeNav = hideTypeNav;
-      $scope.types = documentSrv.types;
-      $scope.selectedDocument = docs[0];
-      $scope.select = function(document){
-          $scope.selectedDocument = angular.copy(document);
-      };
+(function () {
+  'use strict';
+  angular.module('cwl.core')
+    .controller('documentsCtrl', documentsCtrl);
+  documentsCtrl.$inject = ['$scope', 'authSrv', '$location', 'docs', 'documentSrv', 'hideTypeNav'];
+  function documentsCtrl($scope, authSrv, $location, docs, documentSrv, hideTypeNav) {
+    $scope.hideTypeNav = hideTypeNav;
+    $scope.types = documentSrv.types.filter(t => {
+      return docs.length && docs.some(d => t.type === d.type);
+    });
+    $scope.selectedDocument = docs[0];
+    $scope.select = function (document) {
+      $scope.selectedDocument = angular.copy(document);
+    };
 
 
-      $scope.selected = function(document){
-          return document && $scope.selectedDocument && document._id === $scope.selectedDocument._id;
-      };
+    $scope.selected = function (document) {
+      return document && $scope.selectedDocument && document._id === $scope.selectedDocument._id;
+    };
 
-      $scope.filterDocs = {};
-      $scope.documents = docs.filter(function(d) { return !d.expired; });
-      console.log($scope.documents);
-    }
+    $scope.filterDocs = {};
+    $scope.documents = docs.filter(function (d) { return !d.expired; });
+    console.log($scope.documents);
+  }
 }());
