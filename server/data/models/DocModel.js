21fc573e3effcfcbc084156a4dc089b227708f6f
Kyle Curren <ksc4130@gmail.com>
2017-11-14 13:36:16 -0500

    update date parse

--- server/data/models/DocModel.js
+++ server/data/models/DocModel.js
@@ -18,13 +18,13 @@ schema.methods.isPub = function () {
 schema.virtual('expired').
   get(function () {
     const now = moment();
-    const nowForExpires = !this.untilStr ? moment().add(1, 'day') : moment(this.untilStr);
+    const nowForExpires = !this.untilStr ? moment().add(1, 'day') : moment(this.untilStr, 'MM/DD/YYYY');
     return !now.isBetween(moment(new Date(this.when)), nowForExpires);
   });
 
 schema.virtual('until')
   .get(function () {
-    return this.untilStr ? moment(new Date(this.untilStr)) : null;
+    return this.untilStr ? moment(new Date(this.untilStr, 'MM/DD/YYYY')) : null;
   });
 
 module.exports = mongoose.model('Doc', schema);
