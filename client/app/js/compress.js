831e7cdda61f2420f8becb4898fad570561b3fd4
Kyle Curren <ksc4130@gmail.com>
2019-05-09 15:19:07 -0400

    promise

--- client/app/js/compress.js
+++ client/app/js/compress.js
@@ -1,39 +1,56 @@
 var uglify = require('uglify-js');
 var fs = require('fs');
-const prefix = fs.statSync('app.js') ? '' : 'client/app/js/';
 
 function compressJS() {
-  var result = uglify.minify(
-    [
-      prefix + 'app.js',
-      prefix + 'services/*',
-      prefix + 'controllers/*',
-      prefix + 'interceptors/*'
-    ],
-    {
-      outSourceMap: prefix + 'app.min.js.map'
-    }
-  );
+  new Promise((resolve, reject) => {
+    fs.stat('app.js', function(err) {
+      const prefix = err ? 'client/app/js/' : '';
 
-  fs.writeFile('app.min.js', result.code.toString(), function(err) {
-    if (err) {
-      console.log('Error:', err);
-    } else {
-      console.log('Javascript Successfully Compressed!');
-    }
+      var result = uglify.minify(
+        [
+          prefix + 'app.js',
+          prefix + 'services/*',
+          prefix + 'controllers/*',
+          prefix + 'interceptors/*'
+        ],
+        {
+          outSourceMap: prefix + 'app.min.js.map'
+        }
+      );
+
+      Promise.all([
+        new Promise((res, rej) => {
+          fs.writeFile('app.min.js', result.code.toString(), function(err) {
+            if (err) {
+              console.log('Error:', err);
+              rej();
+            } else {
+              console.log('Javascript Successfully Compressed!');
+              res();
+            }
+          });
+        }),
+
+        new Promise((res, rej) => {
+          fs.writeFile(
+            'app.min.js.map',
+            result.map.toString().replace(/public/g, ''),
+            function(err) {
+              if (err) {
+                console.log('Error:', err);
+                rej();
+              } else {
+                console.log('Map Successfully Output!');
+                res();
+              }
+            }
+          );
+        })
+      ])
+        .then(resolve)
+        .catch(reject);
+    });
   });
-
-  fs.writeFile(
-    'app.min.js.map',
-    result.map.toString().replace(/public/g, ''),
-    function(err) {
-      if (err) {
-        console.log('Error:', err);
-      } else {
-        console.log('Map Successfully Output!');
-      }
-    }
-  );
 }
 
 compressJS();
