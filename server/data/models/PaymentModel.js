3e94b8724184b5148080bb22b8f9ad335a9057b7
Kyle Curren <ksc4130@gmail.com>
2019-05-17 17:36:07 -0400

    for and notes

--- server/data/models/PaymentModel.js
+++ server/data/models/PaymentModel.js
@@ -42,7 +42,7 @@ module.exports = mongoose.model('Payment', {
   status: String,
   message: String,
   for: String,
-  note: String,
+  notes: String,
   refId: String,
   refData: String,
   refCard: String,
